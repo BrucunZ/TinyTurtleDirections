723b0b6890bcd983752feb62430fce5fd76c9368
Taufiq <taufiqabdus@gmail.com>
2017-01-18 21:23:26 +0000

    made half a house

---
+++ script.js
@@ -0,0 +1,20 @@
+/*global TinyTurtle*/
+/*global forward*/
+/*global stamp*/
+/*global left*/
+/*global right*/
+/*global down*/
+TinyTurtle.apply(window); 
+forward(20);
+right(90);
+forward(20);
+left(90);
+right(45);
+forward(25);
+left(270);
+stamp();
+
+
+
+
+
